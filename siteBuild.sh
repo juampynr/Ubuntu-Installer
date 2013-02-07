ea536a2bb35d8f765d1e92f00735f0ffa5c70697
Juampy <juampy72@gmail.com>
2013-02-07 13:06:50 +0100

    Added script to create virtual hosts.

---
+++ siteBuild.sh
@@ -0,0 +1,15 @@
+# Creates a virtual host and entry at /etc/hosts for a local site.
+#
+# Usage: sh siteBuild somesite.localhost /path/to/site/root
+# see 'template' file and place it under /etc/apache2/sites-available.
+#
+SITE_HOST=$1
+SITE_PATH=$2
+VIRTUAL_HOST="/etc/apache2/sites-available/$1"
+
+sudo cp /etc/apache2/sites-available/template $VIRTUAL_HOST
+sudo sed -i 's|site_dir|'$SITE_PATH'|g' $VIRTUAL_HOST
+sudo sed -i "s/host_name/$SITE_HOST/g" $VIRTUAL_HOST
+sudo a2ensite $SITE_HOST
+sudo service apache2 reload
+sudo sh -c "echo \"127.0.0.1 $SITE_HOST\" >> /etc/hosts"
